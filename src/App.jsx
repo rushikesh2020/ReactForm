3efe0ae5aa2f5a70a0eeb2dd6fdf07c4e4874e9f
rushikesh2020 <rushikesh.khardekar@gmail.com>
2024-04-03 01:31:29 +0530

    radio buttons in react

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,10 @@ function App() {
       lastName : '',
       email : '',
       comments : '',
-      isFriendly : true
+      isFriendly : true,
+      employment: ''
     }
   )
-
-  const CheckBoxStyle={
-    display: "inline",
-    marginRight: "0.75rem"
-  }
 
   function handleChange(event){
     //destructuring event.target object
@@ -60,7 +56,8 @@ function App() {
         placeholder='Comments'
         value={formData.comments}
       />
-      <input style={CheckBoxStyle}
+      {/* checkbox */}
+      <input 
         type='checkbox'
         id='isFriendly'
         checked = {formData.isFriendly}
@@ -69,6 +66,44 @@ function App() {
       />
       <label htmlFor="isFriendly">Are you friendly?</label>
       <br/>
+    {/* radio buttons */}
+      <fieldset>
+                <legend>Current employment status</legend>
+                
+                <input 
+                    type="radio"
+                    id="unemployed"
+                    name="employment"
+                    value="unemployed"
+                    checked={formData.employment === "unemployed"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="unemployed">Unemployed</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="part-time"
+                    name="employment"
+                    value="part-time"
+                    checked={formData.employment === "part-time"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="part-time">Part-time</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="full-time"
+                    name="employment"
+                    value="full-time"
+                    checked={formData.employment === "full-time"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="full-time">Full-time</label>
+                <br />
+                
+            </fieldset>
 
     </form>
   );
